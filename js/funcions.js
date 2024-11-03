46233d63987318f9e141413e20403fa01008fdac
Siraj-ES <sirajseffarboujemaa@gmail.com>
2024-11-03 15:00:02 +0100

    P2 fet.

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,18 @@
 function confirmaRegistre(){
     //completa
+   
+    const formulari = document.getElementById("formulari")
+
+    formulari.addEventListener('submit', function(event){
+        event.preventDefault();
+        alert("registrant estudiant");
+        console.log("registrant estudiant")
+        const formDiv = document.getElementById("formDiv")   
+        formDiv.innerHTML = "<p class='important'>T'has registrat amb èxit!</p>"
+     });
+     
+    
+
 }
+
+document.addEventListener('DOMContentLoaded', confirmaRegistre);
